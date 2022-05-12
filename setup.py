867b9a9f20432a61b5d16ac8898c946ca21a9fa2
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-12 13:59:20 +0900

    Update setup.py to upload package to pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-top-titlebar-widget',
-    version='0.1.0',
+    version='0.0.1',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
     description='PyQt top title bar widget for frameless window',
     url='https://github.com/yjg30737/pyqt-top-titlebar-widget.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.15',
         'python-color-getter>=0.0.1',
