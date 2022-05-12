30eff20c03d2966809c6ce53e4ca02f666025cf7
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-12 13:54:32 +0900

    Update the url of each package which were recently uploaded to pypi

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         'PyQt5>=5.15',
         'python-color-getter>=0.0.1',
         'pyqt-windows-buttons-widget>=0.0.1',
-        'pyqt-mac-min-max-close-buttons-widget @ git+https://git@github.com/yjg30737/pyqt-mac-min-max-close-buttons-widget.git@main',
+        'pyqt-mac-buttons-widget>=0.0.1',
         'pyqt-svg-icon-text-widget>=0.0.1'
     ]
 )
